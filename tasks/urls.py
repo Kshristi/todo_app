d3350f8defa0be2ff1bf96886f1338c63589135a
shristi <kshristi581@gmail.com>
2026-07-29 19:57:36 +0530

    Added basic features

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns=[
     path('',views.home, name='home'),
+    path('delete/<int:id>/',
+         views.delete_task, name='delete_task'),
+    path('complete/<int:id>/',views.complete_task, name='complete_task'),
+    path('edit/<int:id>/',views.edit_task, name='edit_task'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,36 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
 def home(request):
-    return HttpResponse("Hello")
+    if request.method=="POST":
+        title=request.POST.get("title")
+        Task.objects.create(title=title)
+        return redirect("home")
+    
+    tasks=Task.objects.all()
+
+    context={
+        "tasks":tasks
+    }
+
+    return render(request, "tasks/home.html", context)
+
+def delete_task(request, id):
+    task=get_object_or_404(Task,id=id)
+    task.delete()
+    return redirect('home')
+
+def complete_task(request, id):
+    task=get_object_or_404(Task,id=id)
+    task.completed=True
+    task.save()
+    return redirect('home')
+
+def edit_task(request, id):
+    task=get_object_or_404(Task,id=id)
+
+    if request.method=="POST":
+        task.title=request.POST.get("title")
+        task.save()
+        return redirect('home')
+    return render(request,"tasks/edit.html",{"task":task})
